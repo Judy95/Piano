5a0b9686e1518a62662a28d7e7fb0c7c633107e2
Judyta Zisch <116365516+Judy95@users.noreply.github.com>
2022-11-07 21:39:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 
 const keys = document.querySelectorAll('.key')
 const whiteKeys = document.querySelectorAll('.key.white')
-const blackKeys = document.querySelectorAll('.key.black')
+const blackKeys = document.querySelectorAll('.key.black'
 
 keys.forEach(key => {
   key.addEventListener('click', () => playNote(key))
