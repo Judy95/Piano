38fc031f18613d4f2327b5f07205d27457159c15
Judyta Zisch <116365516+Judy95@users.noreply.github.com>
2022-11-07 21:39:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
 const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 
 const keys = document.querySelectorAll('.key')
-const whiteKeys = document.querySelectorAll('.key.white')
+const whiteKeys = document.querySelectorAll('.key.white'
 const blackKeys = document.querySelectorAll('.key.black')
 
 keys.forEach(key => {
